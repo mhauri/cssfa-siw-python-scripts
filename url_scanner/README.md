7e2ba814f41edb329ee6860facda1882c1ed04c8
Marcel Hauri <marcel.hauri@gmail.com>
2024-01-13 11:30:35 +0100

    [task] add url_scanner

---
+++ url_scanner/README.md
@@ -0,0 +1,11 @@
+# Installation
+```commandline
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt 
+```
+
+# Usage
+```commandline
+python3 url_scanner.py domain.xyz files_or_directories.txt
+```
---
+++ url_scanner/url_scanner.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+
+def check_file_or_directory(url, file_path):
+    with open(file_path, 'r') as file:
+        files_or_directories = [line.strip() for line in file]
+
+    for file_or_directory in files_or_directories:
+        response = requests.get(url + '/' + file_or_directory)
+
+        if response.status_code == 200:
+            print(f"The file or directory {file_or_directory} exists at {url}")
+        else:
+            print(f"Received status code {response.status_code} when checking {file_or_directory} at {url}")
+
+if __name__ == "__main__":
+    url = sys.argv[1]  # Replace with your URL
+    file_path = sys.argv[2]  # Replace with your file path
+
+    check_file_or_directory(url, file_path)
