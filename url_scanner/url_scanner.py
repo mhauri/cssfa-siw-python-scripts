448d70607f2ddea5bcd5316076c5294e26c641f4
Marcel Hauri <marcel.hauri@gmail.com>
2024-01-13 11:39:27 +0100

    [task] update url_scanner

--- url_scanner/url_scanner.py
+++ url_scanner/url_scanner.py
@@ -9,9 +9,11 @@ def check_file_or_directory(url, file_path):
         response = requests.get(url + '/' + file_or_directory)
 
         if response.status_code == 200:
-            print(f"The file or directory {file_or_directory} exists at {url}")
+            print(f"The file or directory {file_or_directory} exists")
+        elif response.status_code == 403:
+            print(f"The file or directory {file_or_directory} exists (403)")
         else:
-            print(f"Received status code {response.status_code} when checking {file_or_directory} at {url}")
+            print(f"Received status code {response.status_code} when checking {file_or_directory}")
 
 if __name__ == "__main__":
     url = sys.argv[1]  # Replace with your URL
